59bcc26252838ab6377c8ff8207b5f4678d52d56
Aditya Poduval <adipoduval5@gmail.com>
2025-02-17 12:03:10 +0530

    made react

---
+++ weak3/src/TextUpdater.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+function TextUpdater() {
+  const [text, setText] = useState("");
+  return (
+    <div>TextUpdater</div>
+  )
+}
+
+export default TextUpdater
