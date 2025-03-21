e65207ea6b1cb2966b1f1fd2378992c92dbe1362
Aditya Poduval <adipoduval5@gmail.com>
2025-03-22 00:00:10 +0530

    working on app.jsx

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
     <h1 className="text-3xl font-bold text-red-600">Tailwind CSS is working! 🚀</h1>
+    <h1 className="text-3xl font-bold">Hello there</h1>
   </div>
 );
 }
