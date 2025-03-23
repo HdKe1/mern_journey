508ea8694da91860ca235c44aef821cdb27d5f81
Aditya Poduval <adipoduval5@gmail.com>
2025-03-24 01:00:37 +0530

    done with week3

--- week3/src/LoginFrom.jsx
+++ week3/src/LoginFrom.jsx
@@ -12,20 +12,21 @@ const LoginForm = () => {
   return (
     <div className="flex flex-col items-center mt-5">
       <form onSubmit={handleSubmit} className="flex flex-col items-center bg-white p-6 rounded-lg shadow-md w-80">
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="p-2 mb-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="p-2 mb-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+      <input
+  type="email"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  placeholder="Email"
+  className="p-2 mb-3 w-full border border-gray-300 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+/>
+<input
+  type="password"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+  placeholder="Password"
+  className="p-2 mb-3 w-full border border-gray-300 rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+/>
+
         <button
           type="submit"
           className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 w-full"
--- week3/src/main.jsx
+++ week3/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Button from './Button'
+import Form from src/Form.jsx
+import TextState from src/Textstate.jsx
 import LoginForm from './LoginFrom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <LoginForm></LoginForm>
+   
   </StrictMode>
 )
